d87b26ec3990c5192b6c11843198ea6b29a5dde9
belkisarslan <belkisarslan33@gmail.com>
2023-01-09 22:48:32 +0300

    color palette

--- src/features/canvas/Canvas.js
+++ src/features/canvas/Canvas.js
@@ -2,22 +2,59 @@ import React, { useState } from 'react'
 import  '../../index.css'
 
 
+
 export function Canvas() {
-    const [points, setPoints] = useState([])
-    const clickHandle = e =>{
-     setPoints(points => [...points,{
-        x: e.clientX,
-        y: e.clientY
-     }])
-    }
+  const types = [
+    {
+      color:'black',
+      text: 'black'
+    },
+    {
+      color:'orange',
+      text: 'orange'
+    },
+    {
+      color:'blue',
+      text: 'blue'
+    },
+    {
+      color:'pink',
+      text: 'pink'
+    },
+    {
+      color:'green',
+      text: 'green'
+    },
+    {
+      color:'yellow',
+      text: 'yellow'
+    },
+ ]
+ const [color, setColor] = useState(types[0].color)
+ const [points, setPoints] = useState([])
+ const clickHandle = e =>{
+    setPoints(points => [...points,{
+      x: e.clientX,
+      y: e.clientY
+  }])
+}
   
+  const changeColor = (e) => {
+    setColor(e.target.value)
+  }
+  console.log(color)
   return (
     <div>
       <div className='canvas' onClick={clickHandle}>
         {points.map((point,key) => (
-            <div key={key} className='point' style={{left: point.x, top:point.y}}></div>
+            <div className='point' key={key} style={{'--color':color, left: point.x, top:point.y}}></div>
         ))}
       </div>
+      <select style={{'--color':color}} onChange={changeColor}>
+        {types.map((type, key) => (
+          <option style={{'--color':color}} key={key} value={type.color}>{type.text}</option>
+        ))}
+      </select>
     </div>
   )
 }
